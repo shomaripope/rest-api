167006196fab1a26b8fc348b1d4c7fae5c852cf2
Shomari Pope <shomaripope@gmail.com>
2019-05-12 11:19:41 -0400

    created geojson functionallity and front end

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 mongoose.connect('mongodb://localhost/ninjago', {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 
+app.use(express.static('public'));
+
 app.use(bodyParser.json());
 
 //initialize routes
